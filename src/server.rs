97bf9577ac006d74bb5a74d8175253630b235e5a
ABHISHEK <ay22oct02@gmail.com>
2025-06-25 13:20:23 +0530

    mutiple connection using threadpool

--- src/server.rs
+++ src/server.rs
@@ -9,16 +9,15 @@ use crate::lib::StatusCode;
 use crate::req;
 use crate::res::HttpResponse;
 use crate::util::parse_incoming_req;
+use threadpool::ThreadPool;
 
 pub fn start_tcp() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    // print!("1  ");
+    let pool = ThreadPool::new(15);
     for stream in listener.incoming() {
-        // print!("2  ");
         match stream {
             Ok(_stream) => {
-                handle_connection(_stream);
-                // println!("accepted new connection");
+                pool.execute(|| handle_connection(_stream));
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -28,10 +27,8 @@ pub fn start_tcp() {
 }
 
 fn handle_connection(stream: TcpStream) {
-    // let request_line: Vec<&str> = http_request.get(0).unwrap().split(" ").collect();
     let res_stream = stream.try_clone().expect("failed stream cloning");
     let mut req = parse_incoming_req(stream);
-    let response = "HTTP/1.1 404 Not Found\r\n\r\n";
     let req_headers = req.headers.clone();
     let mut res = HttpResponse::new(StatusCode::Ok, req_headers, None, res_stream);
     route_handler(&mut req, &mut res);
